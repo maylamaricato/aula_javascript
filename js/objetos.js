15e07512896180c5e7b0dd7bbbd5576db72ced26
maylamaricato <162464230+maylamaricato@users.noreply.github.com>
2024-04-05 09:43:45 -0300

    Add files via upload

---
+++ js/objetos.js
@@ -0,0 +1,15 @@
+var aluno1 = {
+    ra: 43578,
+    nome: 'Jose da Silva',
+    curso: 'Engenharia de Software',
+    ativo: true
+};
+
+console.log(aluno1.ra);
+console.log(aluno1.nome);
+console.log(aluno1.curso);
+console.log(aluno1.ativo);
+
+aluno1.datanasc = '17/12/2005';
+
+console.log(aluno1.datanasc);
